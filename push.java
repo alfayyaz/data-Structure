514b196aaf8eb4a5d0fa03dc2fb8f868c8ffb58b
Ali <ar8905226@gmail.com>
2024-09-19 00:07:33 +0500

    new one

--- push.java
+++ push.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+
+
 public class push {
     public static void main(String[] args) {
-        System.out.println("hello");
+Scanner scanner=new Scanner(System.in);
+System.out.println("Enter your stack size");
+int input=scanner.nextInt();
+Stack<Integer> stack=new Stack<>();
+System.out.println("Enter your data to stack");
+for(int i=0;i<input;i++){
+  
+    int number=scanner.nextInt();
+    if(stack.size()<input){
+stack.push(number);
+
+
     }
-    
+    else {
+        System.out.println("stack is full");
+    }
 }
+for(Integer element:stack){
+    System.out.println(element);
+}
+    
+}}

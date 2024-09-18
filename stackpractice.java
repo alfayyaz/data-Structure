77de0880ea9fb49e20a479706d106e7f1212f546
Ali <ar8905226@gmail.com>
2024-09-18 20:31:44 +0500

    new task

---
+++ stackpractice.java
@@ -0,0 +1,9 @@
+import java.util.Stack;
+
+public class stackpractice{
+    public static void main(String[] args) {
+       Stack<Integer> stack=new Stack<>();
+       stack.push(65643);
+      System.out.println( stack.pop());
+    }
+}
